b2a790c96d2d7bd4bda0f27e727dde76938c1715
Kareem727 <kareem.ga.13@gmail.com>
2022-02-16 11:01:53 +0200

    second  commit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,59 +1,88 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
-const SimpleInput = (props) => { 
-  const nameInputRef = useRef();
-  const [currentValue, setCurrentValue] = useState('');
-  const [validTouched, setValidtouched] = useState();
+const SimpleInput = (props) => {
   
-   const setValed = currentValue.trim() !== '';
-   const setNamedValidTouched = validTouched && !valid;
+  
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const [enteredName, setCurrentName] = useState('');
+  const [validTouched, setValidtouched] = useState(true);
+  
+  const setValed = enteredName.trim() !== '';
+  const setNamedValidTouched = validTouched && !setValed;
+  const setCurrentClass = setNamedValidTouched ? 'form-control invalid' : 'form-control' ;
+
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmailTouched, setenteredEmailTouched] = useState(false);
+
+  const setValidEmail = enteredEmail.includes('@');
+  const setEmailisInvalid = !setValidEmail && enteredEmailTouched;
+  const setClassemail = setEmailisInvalid ? '':'';
+
+  
+  let valueValid = false;
+  if(enteredName){
+    valueValid =true;
+   }
+
+   const printemail =(event)=>{
+     setEnteredEmail(event.target.value);
+   }
+
+
 
 
   const printValue =(event) =>{
-    setCurrentValue(event.target.value);
+    setCurrentName(event.target.value);
 
   }
   
-  const setCurrentClass = setNamedValidTouched ? 'form-control invalid' : 'form-control' ;
+
   const onSupmitHandler =(event)=>{
     event.preventDefault();
-    setValidtouched(true);
+    setValidtouched(false);
+    setenteredEmailTouched(false);
 
-
-    
-
-    
-    
-  //   const entededVVV = nameInputRef.current.value;
-  // console.log(entededVVV);
-   console.log(currentValue);
-   const currentName = nameInputRef.current.value;
-   console.log(currentName);
-   setCurrentValue('');
+   const enteredName = nameInputRef.current.value;
+   console.log(enteredName);
+   const enteredEmail = emailInputRef.current.value;
+   console.log(enteredEmail);
+   setEnteredEmail('');
 
   }
 
   const onBlureHandler =() =>{
     setValidtouched(true);
-
-    if(currentValue.trim() === '' ){
-      return setValed(false);      
-      }else{
-        setValed(true);
+    setenteredEmailTouched(true);
+    if(!setValed){
+      return;      
       }
 
   }
 
   return (
     <form onSubmit={onSupmitHandler}>
+
       <div className={setCurrentClass}>
         <label htmlFor='name'>Your Name</label>
-        <input onBlur={onBlureHandler} ref={nameInputRef} type='text' id='name' onChange={printValue} value={currentValue} />
+        <input onBlur={onBlureHandler} ref={nameInputRef} type='text' id='name' onChange={printValue} value={enteredName} />
         {setNamedValidTouched && <p className="error-text">You Must Fill The input</p>}
+      </div>
+
+      <div className={setCurrentClass}>
+        <label htmlFor='email'>Your E-Mail</label>
+        <input onBlur={onBlureHandler} 
+          ref={emailInputRef}
+          type='email'
+          id='email'
+          onChange={printemail}
+          value={enteredEmail} />
+
+        {setNamedValidTouched && <p className="error-text">please enter a valid email</p>}
       </div>
       <div className="form-actions">
       
-        <button>Submit</button>
+        <button disabled={!valueValid}>Submit</button>
       </div>
     </form>
   );
